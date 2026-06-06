ea1705c7c38d7bfad3982631272ca1a497246b2f
LeonardoRibeiro-cpu <leonardo.ribeiro@estudante.iftm.edu.br>
2026-06-06 17:35:33 -0300

    correcoes botoes

--- app01/src/componentes/FaleComigo/FaleComigo.jsx
+++ app01/src/componentes/FaleComigo/FaleComigo.jsx
@@ -3,7 +3,7 @@ import styles from "./FaleComigo.module.css";
 function FaleComigo() {
     return ( 
         <>
-            <button type="button" className={styles.button}>Fale comigo -{">"}</button>
+            <button type="button" className={styles.button}>Fale comigo →</button>
         </>
      );
 }
