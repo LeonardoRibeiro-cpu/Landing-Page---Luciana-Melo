b98617c8c8b715b6ea5cfbc2f514207a4eaeee7f
GiiovannyMorais <giovanny.cabral@estudante.iftm.edu.br>
2026-06-13 16:25:21 -0300

    Alterações feitas

--- app01/src/App.jsx
+++ app01/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import Formulario from './componentes/Formulario/Formulario';
+import Formulario from './main/Sessao7 - Giovanny/Formulario/Formulario';
 import Header from './header/Header';
 import './css/style.css';
 import Footer from './footer/Footer';
---
+++ app01/src/main/Sessao7 - Giovanny/Sessao7.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Sessao7() {
+    return (
+        <></>
+    );
+}
+
+export default Sessao7;
